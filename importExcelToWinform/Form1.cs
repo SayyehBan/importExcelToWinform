fba0fdcededc83ad47ce0b06dcfc74af8af181f8
SayyehBan <sdvp1991david@gmail.com>
2023-05-29 15:46:27 +0330

    Add project files.

---
+++ importExcelToWinform/Form1.cs
@@ -0,0 +1,47 @@
+﻿using ExcelDataReader;
+using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace importExcelToWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Excel Files|*.xlsx;*.xls;*.xlsm";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+
+                // Create an instance of FileStream to read from the Excel file
+                using (FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    // Create an instance of IExcelDataReader to read the Excel file with 
+                    using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        // Load the contents of the Excel file into a DataTable
+                        DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                        {
+                            ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                            {
+                                UseHeaderRow = true
+                            }
+                        });
+
+                        // Set the first table in the DataSet as the source of the DataGridView
+                        dataGridView1.DataSource = result.Tables[0];
+                    }
+                }
+            }
+        }
+    }
+}
